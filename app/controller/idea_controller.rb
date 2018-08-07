5ae6b98585ed7ad91b68a356b9a464a8b6c83f73
Kachel Wittig <kjwittig@gmail.com>
2018-08-07 00:18:40 -0500

    get post new ideas route completed

--- app/controller/idea_controller.rb
+++ app/controller/idea_controller.rb
@@ -1,7 +1,30 @@
 class IdeaController < ApplicationController
 
   get '/ideas' do
-    erb :'/ideas/all_ideas'
+    if logged_in?
+      @user = current_user
+      @ideas = Idea.all
+      erb :'/ideas/all_ideas'
+    else
+      redirect to '/login'
+    end
   end
 
+  get '/ideas/new' do
+    if logged_in?
+      @user = current_user
+      erb :'ideas/new_idea'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/ideas' do
+    if !params[:project].empty?
+      @idea = Idea.create(project: params[:project], user_id: current_user.id)
+      erb :'/ideas/show'
+    else
+      redirect '/ideas/new'
+    end
+  end
 end
